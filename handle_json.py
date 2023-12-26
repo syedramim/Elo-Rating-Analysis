d7ec59bd9ea201d9823cc8ef4186c6b9ec2eefe0
Syed Ramim <parthib004@gmail.com>
2023-12-26 17:38:27 -0500

    added typings

--- handle_json.py
+++ handle_json.py
@@ -1,19 +1,24 @@
 import json
 import requests
 
-def parse_json(url):
+def parse_json(url, isInfo):
     response = requests.get(url)
     content = response.json()
     
-    name = content['name']
-    moveset = ', '.join([move['move']['name'] for move in content['moves']])
-    types = ', '.join([p_type['type']['name'] for p_type in content['types']])
-    stats = [stat['base_stat'] for stat in content['stats']]
-    
-    poke_info = {'NAME': name , 'TYPE': types, 'HP': stats[0], 'ATTACK': stats[1], 'DEFENSE': stats[2], 
-                                'SPECIAL-ATTACK': stats[3], 'SPECIAL-DEFENSE': stats[4], 'SPEED': stats[5], 
-                                'TOTAL': sum(stats), 'MOVESET': moveset}
-    
+    if isInfo:
+        name = content['name']
+        moveset = ', '.join([move['move']['name'] for move in content['moves']])
+        types = ', '.join([p_type['type']['name'] for p_type in content['types']])
+        stats = [stat['base_stat'] for stat in content['stats']]
+        
+        poke_info = {'NAME': name , 'TYPE': types, 'HP': stats[0], 'ATTACK': stats[1], 'DEFENSE': stats[2], 
+                                    'SPECIAL-ATTACK': stats[3], 'SPECIAL-DEFENSE': stats[4], 'SPEED': stats[5], 
+                                    'TOTAL': sum(stats), 'MOVESET': moveset}
+    else:
+        p_type_name = content['name']
+        poke_info = {p_type_name: {relation: [name['name'] for name in content['damage_relations'][relation]] 
+                                   for relation in content['damage_relations']}}
+        
     return poke_info
 
 def main():
